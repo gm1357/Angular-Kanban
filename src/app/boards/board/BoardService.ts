6258800f4f7cf051b5fc77303c131e950db36017
Gabriel Machado <g_machado1@hotmail.com>
2019-06-28 20:55:17 -0300

    Corrigido bug ao iniciar aplicação pela primeira vez

--- src/app/boards/board/BoardService.ts
+++ src/app/boards/board/BoardService.ts
@@ -1,53 +1,52 @@
 import { Injectable, OnInit } from '@angular/core';
 import { Board } from './board';
 
+const BOARDS_KEY = 'boards';
+const CURRENT_ID_KEY = 'currentBoardId';
+
 @Injectable({ providedIn: 'root' })
 export class boardService {
 
     private boardsList: Board[] = [];
-    private currentId: number;
+    private currentId: number = 0;
 
     constructor() {
-        this.boardsList = JSON.parse(window.localStorage.getItem('boards'));
-        this.currentId = new Board(this.boardsList[0]).id;
+        this.boardsList = JSON.parse(window.localStorage.getItem(BOARDS_KEY)) || [];
+        this.currentId = new Board(this.boardsList[0]).id || 0;
 
-        if (!this.boardsList) {
-            this.boardsList = [];
-            this.currentId = 0;
-        }
+        window.localStorage.setItem(CURRENT_ID_KEY, this.currentId.toString());
     }
 
     hasBoards() {
-        return !!window.localStorage.getItem('boards');
+        return !!window.localStorage.getItem(BOARDS_KEY);
     }
 
     setBoard(board: Board) {
-        console.log(this.boardsList);
         this.boardsList.unshift(board);
         this.currentId = board.id;
         
-        window.localStorage.setItem('currentBoardId', this.currentId.toString());
-        window.localStorage.setItem('boards', JSON.stringify(this.boardsList));
+        window.localStorage.setItem(CURRENT_ID_KEY, this.currentId.toString());
+        window.localStorage.setItem(BOARDS_KEY, JSON.stringify(this.boardsList));
     }
 
     getBoard(id: number): Board {
-        return new Board(JSON.parse(window.localStorage.getItem('boards')).find(board => board._id == id));
+        return new Board(JSON.parse(window.localStorage.getItem(BOARDS_KEY)).find(board => board._id == id));
     }
 
     getCurrentId() {
-        return parseInt(window.localStorage.getItem('currentBoardId'));
+        return parseInt(window.localStorage.getItem(CURRENT_ID_KEY));
     }
 
     getAllBoards(): Board[] {
-        const boards: Board[] = JSON.parse(window.localStorage.getItem('boards')).map(obj => new Board(obj));
+        const boards: Board[] = JSON.parse(window.localStorage.getItem(BOARDS_KEY)).map(obj => new Board(obj));
 
-        if (boards.length) {
+        if (boards.length >= 0) {
             this.currentId = boards[0].id;
         } else {
             this.currentId = 0;
         }
 
-        window.localStorage.setItem('currentBoardId', this.currentId.toString());
+        window.localStorage.setItem(CURRENT_ID_KEY, this.currentId.toString());
 
         return boards;
     }
@@ -60,6 +59,6 @@ export class boardService {
         
         this.boardsList.splice(index, 1);
         
-        window.localStorage.setItem('boards', JSON.stringify(this.boardsList));
+        window.localStorage.setItem(BOARDS_KEY, JSON.stringify(this.boardsList));
     }
 }
